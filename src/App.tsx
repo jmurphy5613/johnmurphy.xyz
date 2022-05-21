c04904bb9146b2a18e38d74ef9d62a6013f7e565
jmurphy5613 <jmurphy5613@gmail.com>
2022-05-21 15:23:05 -0400

    created new template with jsx elements

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App" style={{
-      height: '100vh',
-      width: '100vw',
-      backgroundColor: '#000000'
-    }}>
-      <Navbar />
+    <div className="App">
+      <ThreeScene />
+      <div className="main-content">
+        <h1> 
+          <h3>
+            Hey there, I'm John.
+          </h3>
+          I'm a passionate 
+          <Typed style={{ margin: '0 0 1rem 0'  }} typeSpeed={90} backSpeed={50} backDelay={10} loop strings={['full stack developer', 'web3 engineer', 'blockchain developer']} />
+          <h4>I'm currently a developer at Landkit, a software company that creates algorithmic solutions to landscaping projects</h4>
+        </h1>
+        
+      </div>
     </div>
   );
 }
