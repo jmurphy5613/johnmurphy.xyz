1bd4e10bdca8ec2555fc8e07fc032cdc0fc5589a
jmurphy5613 <jmurphy5613@gmail.com>
2022-12-04 00:55:28 -0500

    added little cool message

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,21 +8,31 @@ const Home = () => {
         <div className="App">
             <ThreeScene />
             <a href="https://github.com/jmurphy5613">
-            <img src="/github.png" className="github" />
+                <img src="/github.png" className="github" />
             </a>
-    
+
             <div className="main-content">
-            <h1>
-                <h3>
-                Hey there, I'm John.
-                </h3>
-                I'm a passionate 
-                <Typed style={{ margin: '0 0 1rem 0'  }} typeSpeed={90} backSpeed={50} backDelay={10} loop strings={['full stack developer', 'frc competitor']} />
-                <h4>I'm currently a developer at Landau Design+Technology, a company that creates algorithmic solutions to landscaping projects</h4>
-            </h1>
+                <h1>
+                    <h3>Hey there, I'm John.</h3>
+                    I'm a passionate
+                    <Typed
+                        style={{ margin: "0 0 1rem 0" }}
+                        typeSpeed={90}
+                        backSpeed={50}
+                        backDelay={10}
+                        loop
+                        strings={["full stack developer", "frc competitor"]}
+                    />
+                    <h4>
+                        I'm currently a developer at Landau Design+Technology, a
+                        company that creates algorithmic solutions to
+                        landscaping projects
+                    </h4>
+                    <h5>{`-> look on desktop (it's cooler) <-`}</h5>
+                </h1>
             </div>
         </div>
-    )
+    );
 }
 
 export default Home;
